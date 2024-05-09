395c942414083982de442abbaefeaf5212791ca3
Adelmysterio <storliniadelmo@gmail.com>
2024-05-09 15:38:03 +0200

    stylized hmtl and achieved Milestone 2

--- js/script.js
+++ js/script.js
@@ -4,52 +4,64 @@ const underDiscount = 24.552;
 
 const overDiscount = 37.893;
 
-const bottone = document.querySelector('button')
+const bottone = document.querySelector('button');
 
 bottone.addEventListener('click', function () {
 
-    const km = document.querySelector('input').value
+    const km = Number.parseInt(document.querySelector('input').value);
 
-    const age = document.querySelector('#age').value
+    if (isNaN(km)) {
+
+        alert('Valore chilometri non valido')
+
+    };
+
+    const age = Number.parseInt(document.querySelector('#age').value);
+
+    if (isNaN(age)) {
+
+        alert('Valore età non valido')
+
+    };
 
     let track = km * priceRaw;
-    
-    let trackShow = track.toFixed(2)
-    
-    document.getElementById('track').innerHTML = trackShow
-    
+
+    let trackShow = track.toFixed(2);
+
+    document.getElementById('track').innerHTML = '&euro; ' + trackShow
+
     if (age < 21) {
-    
+
         let priceDiscounted = (track / 100) * underDiscount
-    
+
         let priceFinal = track - priceDiscounted
-    
+
         let priceFinalShow = priceFinal.toFixed(2)
-    
+
         console.warn(priceFinalShow)
-    
-        document.getElementById('priceFinalShow').innerHTML = priceFinalShow
+
+        document.getElementById('priceFinalShow').innerHTML = '&euro; ' + priceFinalShow
     }
-    
+
     if (age >= 63) {
-    
+
         let priceDiscounted = (track / 100) * overDiscount;
-    
+
         let priceFinal = track - priceDiscounted;
-    
+
         let priceFinalShow = priceFinal.toFixed(2);
-    
+
         console.warn(priceFinalShow)
-    
-        document.getElementById('priceFinalShow').innerHTML = priceFinalShow
+
+        document.getElementById('priceFinalShow').innerHTML = '&euro; ' + priceFinalShow
     }
-    
+
     else if (age >= 21 && age < 63) {
-    
+
         let priceFinalShow = track.toFixed(2)
-    
+
         console.warn(priceFinalShow)
-    
-        document.getElementById('priceFinalShow').innerHTML = priceFinalShow
+
+        document.getElementById('priceFinalShow').innerHTML = '&euro; ' + priceFinalShow
     };
 })
